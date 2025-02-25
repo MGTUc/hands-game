cb2babf926c672a82d74ba602602c3d69becd7a5
MGerror137 <maarten.geybels@hotmail.com>
2025-02-25 23:12:24 +0100

    created directional graph building

--- DirectedGraph.mjs
+++ DirectedGraph.mjs
@@ -2,18 +2,21 @@ export class DirectedGraph {
     vertices;
     rootVertex;
     /**
-     * @param {Vertex} root 
+     * @param {Vertex} root
      */
     constructor(root) {
-        this.vertices={[String(root.position)+String(root.computerturn)]:root};
+        this.vertices = {
+            [root.position + String(root.computerturn)]: root,
+        };
         this.rootVertex = root;
     }
     getRoot() {
         return this.rootVertex;
     }
-    addVertex(vertices) {
-        for (let vertex of vertices) {
-            this.vertices[(vertex.position,vertex.computerturn)] = vertex;
+    addVertex(...vertices) {
+        for (let vertex of vertices.flat()) {
+            this.vertices[vertex.position + String(vertex.computerturn)] =
+                vertex;
         }
     }
 }
@@ -24,8 +27,8 @@ export class Vertex {
     computerturn;
     children;
     /**
-     * 
-     * @param {number} position represents the position eg, 1111 1112
+     *
+     * @param {String} position represents the position eg, 1111 1112
      * @param {number} value represents the value of the position (a value between -1 and 1, 1 being the computer and -1 the player)
      * @param {boolean} computerturn true for computer, false for player
      * @param {Vertex[]} children array of child nodes of type Node
--- tests.mjs
+++ tests.mjs
@@ -1,7 +1,80 @@
-import { DirectedGraph,Vertex } from './DirectedGraph.mjs';
+import { DirectedGraph, Vertex } from "./DirectedGraph.mjs";
 
-let startingVertex = new Vertex(1111,0,false);
+let startingVertex = new Vertex(1111, 0, false);
 let graph = new DirectedGraph(startingVertex);
-assert(graph.getRoot().position === 1111);
-assert(graph.getRoot().value === 0);
-assert(graph.getRoot().computerturn === false);
+let reachedAllPositions = false;
+let frontier = [graph.getRoot()];
+let count = 0;
+while (!reachedAllPositions) {
+    let newFrontier = [];
+    for (let vertex of frontier) {
+        let children = generateChildren(vertex);
+        for (let child of children) {
+            if (!graph.vertices[child.position + String(child.computerturn)]) {
+                graph.addVertex(child);
+                if (child.value === 0) {
+                    newFrontier.push(child);
+                } else {
+                    count++;
+                    console.log(
+                        child.position +
+                            String(child.computerturn) +
+                            String(child.value)
+                    );
+                }
+            }
+        }
+    }
+    frontier = newFrontier;
+    reachedAllPositions = frontier.length === 0;
+}
+console.log(graph.vertices);
+console.log(Object.keys(graph.vertices).length);
+console.log(count);
+
+function generateChildren(vertex) {
+    let children = [];
+    let positions = new Set();
+    let [a1, a2, a3, a4] = vertex.position.toString().split("").map(Number);
+    positions.add(
+        String([Math.min(a3 + a1, 5) % 5, a4].sort().concat([a1, a2]))
+            .split(",")
+            .slice(0, 4)
+            .join("")
+    );
+    positions.add(
+        String([Math.min(a3 + a2, 5) % 5, a4].sort().concat([a1, a2]))
+            .split(",")
+            .slice(0, 4)
+            .join("")
+    );
+    positions.add(
+        String([Math.min(a4 + a1, 5) % 5, a3].sort().concat([a1, a2]))
+            .split(",")
+            .slice(0, 4)
+            .join("")
+    );
+    positions.add(
+        String([Math.min(a4 + a2, 5) % 5, a3].sort().concat([a1, a2]))
+            .split(",")
+            .slice(0, 4)
+            .join("")
+    );
+    for (let position of positions) {
+        let value;
+        let computerturn = !vertex.computerturn;
+        if (position.startsWith("00")) {
+            if (computerturn) {
+                value = -1;
+            } else {
+                value = 1;
+            }
+        } else {
+            value = 0;
+        }
+        let child = new Vertex(position, value, computerturn);
+        vertex.children.push(child);
+        children.push(child);
+    }
+    return children;
+}
